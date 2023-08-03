8a1c76f75d63adc10856aaf59515e52acc558f82
hikmetsekerc <hikmetsekerci0@gmail.com>
2023-08-03 16:22:00 -0400

    commit second

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,15 @@ public class Main {
         System.out.printf("Hello and welcome!");
 
         System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!");
 
 
 
 
-   }
+
+
+    }
 
 
 
